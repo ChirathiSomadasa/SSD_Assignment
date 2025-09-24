0af8b0404d7b633e47bfe29436acee4cce29a31b
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-09-24 09:35:45 +0530

    update

--- server/passport.js
+++ server/passport.js
@@ -48,28 +48,3 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-/*const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const passport = require("passport");
-
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: "http://localhost:5001/auth/google/callback",
-			scope: ["profile", "email"],
-		},
-		function (accessToken, refreshToken, profile, callback) {
-			callback(null, profile);
-		}
-	)
-);
-
-passport.serializeUser((user, done) => {
-	done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-	done(null, user);
-});*/
-
